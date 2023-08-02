3a4f552ed8811a7dbe12c66582e10917aaf01dca
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2023-08-02 09:25:00 -0400

    Let package manager know what is included in repo

---
+++ repo.py
@@ -0,0 +1,21 @@
+# LUMEN v1.00, CopyLeft 2023 Sean Benware
+# List of all packages supplied by this repo
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+"""
+
+packages = [
+    'test', 'neofetch', 'another_test_package'
+]
