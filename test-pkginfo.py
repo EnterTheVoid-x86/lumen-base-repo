a82bf4c9cbff9c93c30d0559ffbbdfe757c843a8
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2023-08-02 09:58:48 -0400

    Update test-pkginfo.py

--- test-pkginfo.py
+++ test-pkginfo.py
@@ -1,3 +1,3 @@
 pkgname = "Test package"
 pkgversion = "v1.0.1"
-creates = ["/usr/bin/test_command", "/usr/bin/another_file_for_testing", "/usr/bin/thisdoesntexisteither"]
+creates = ["/usr/bin/test_command", "/usr/bin/another_file_for_testing"]
